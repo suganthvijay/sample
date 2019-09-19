cd480b80ff7f7ad197411241c59906d99f2f1fb6
hackerman1337 <hackerman1337@hack.me>
2019-09-19 15:46:23 +0530

    updated the routes

--- app.js
+++ app.js
@@ -3,13 +3,25 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 
-app.use('/', express.static(__dirname)).listen(port, () => {
+app.use(express.static(__dirname)).listen(port, () => {
     console.log(`listening on port ${port}`);
 });
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/blog.html');
 });
-app.post('/signup.html' , (req, res) => {
+
+app.get('/login' , (req, res) => {
     res.sendFile(__dirname + '/signup.html');
+});
+app.post('/login' , (req, res) => {
+    res.sendFile(__dirname + '/blog.html');
+});
+
+//Sign Up Page
+app.get('/signup' , (req, res) => {
+    res.sendFile(__dirname + '/signup.html');
+});
+app.post('/signup' , (req, res) => {
+    res.sendFile(__dirname + '/blog.html');
 });
