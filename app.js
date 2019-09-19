6288f9012cec7a399613719cbd72646dd4454dcd
hackerman1337 <hackerman1337@hack.me>
2019-09-19 15:51:06 +0530

    changed

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 
-app.use(express.static(__dirname)).listen(port, () => {
+app.use('/', express.static(__dirname)).listen(port, () => {
     console.log(`listening on port ${port}`);
 });
 
