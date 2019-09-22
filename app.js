8eac43b08b3bc6f8bf9729fcaf84c800870f0763
VikNash7 <viknash97@gmail.com>
2019-09-22 20:16:36 +0530

    mistakes rectified

--- app.js
+++ app.js
@@ -10,21 +10,19 @@ app.use('/', express.static(__dirname)).listen(port, () => {
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/blog.html');
 });
-app.post('/', (req, res) => {
-    res.sendFile(__dirname + '/blog.html');
-});
+
 //login  page
 app.get('/login' , (req, res) => {
     res.sendFile(__dirname + '/login.html');
 });
-// app.post('/login' , (req, res) => {
-//     res.sendFile(__dirname + '/blog.html');
-// });
+app.post('/login' , (req, res) => {
+    res.sendFile(__dirname + '/blog.html');
+});
 
 //Sign Up Page
 app.get('/signup' , (req, res) => {
     res.sendFile(__dirname + '/signup.html');
 });
-// app.post('/signup' , (req, res) => {
-//     res.sendFile(__dirname + '/blog.html');
-// });
+app.post('/signup' , (req, res) => {
+    res.sendFile(__dirname + '/blog.html');
+});
